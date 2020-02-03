15e0cbc1e40cbc7333483d5a25dc1670a5ff1b33
righ <righ.m9@gmail.com>
2020-02-03 09:54:23 +0900

    chat2

---
+++ chat2/chat_client/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	pb "github.com/righ/grpc-go-example/chat2/chat2"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "grpc-server:50051"
+)
+
+func main() {
+	var (
+		name = flag.String("name", "noname", "user name")
+	)
+	flag.Parse()
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewChatClient(conn)
+	ctx := context.Background()
+	user, err := c.CreateChannel(ctx, &pb.Null{})
+	if err != nil {
+		log.Fatalf("could not talk: %v", err)
+		return
+	}
+	go getMessage(c, user.Id)
+
+	cli, err := c.Talk(ctx)
+	if err != nil {
+		log.Fatalf("could not talk: %v", err)
+		return
+	}
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			message := scanner.Text()
+			if message == "" {
+				continue
+			}
+			if err := cli.Send(&pb.MessageRequest{Id: user.Id, Name: *name, Message: message}); err != nil {
+				log.Fatalf("Send failed: %v", err)
+			}
+		}
+	}()
+	for {
+		reply, err := cli.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("could not chat: %v", err)
+		}
+		log.Printf("%s> %s", reply.Name, reply.Message)
+	}
+}
+
+func getMessage(c pb.ChatClient, uid uint64) error {
+	stream, err := c.GetMessages(context.Background(), &pb.User{Id: uid})
+	if err != nil {
+		return err
+	}
+	for {
+		reply, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf("%s> %s", reply.Name, reply.Message)
+	}
+	return nil
+}
---
+++ chat2/chat_server/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"time"
+
+	pb "github.com/righ/grpc-go-example/chat2/chat2"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type message struct {
+	name string
+	text string
+	time time.Time
+}
+
+var serial = 1
+var channels = make(map[uint64]chan message)
+
+type server struct {
+	pb.UnimplementedChatServer
+}
+
+func (s *server) CreateChannel(_ context.Context, u *pb.Null) (*pb.User, error) {
+	serial++
+	channels[uint64(serial)] = make(chan message)
+	return &pb.User{Id: uint64(serial)}, nil
+}
+
+func (s *server) Talk(srv pb.Chat_TalkServer) error {
+
+	for {
+		req, err := srv.Recv()
+		if err != nil {
+			return err
+		}
+		name, msg := req.GetName(), req.GetMessage()
+		log.Printf("%s>: %s", name, msg)
+		now := time.Now()
+
+		for id, c := range channels {
+			if id == req.Id {
+				continue
+			}
+
+			go func(c chan message) { c <- message{name, msg, now} }(c)
+		}
+	}
+}
+
+func (s *server) GetMessages(user *pb.User, srv pb.Chat_GetMessagesServer) error {
+	//lastRead := time.Now()
+
+	for {
+		m := <-channels[user.Id]
+		srv.Send(&pb.MessageReply{Name: m.name, Message: m.text})
+
+	}
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterChatServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
